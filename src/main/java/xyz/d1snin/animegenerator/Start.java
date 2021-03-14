93b85edc17d4c773d366f2fc137ad9e4fa62962d
d1snin <d1snin@aol.com>
2021-03-15 00:10:44 +0300

    [chore] update Start.java

--- src/main/java/xyz/d1snin/animegenerator/Start.java
+++ src/main/java/xyz/d1snin/animegenerator/Start.java
@@ -6,9 +6,9 @@ public class Start {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome!");
-        System.out.print("Please enter a name for the JSON file to be generated: ");
+        System.out.println("Please enter a name for the JSON file to be generated: ");
         String fileName = scanner.nextLine();
-        System.out.print("Please enter the number of created links for your file: ");
+        System.out.println("Please enter the number of created links for your file: ");
         int strings = scanner.nextInt();
         System.out.println("Generating...");
         Generator.Generate(strings, fileName);
