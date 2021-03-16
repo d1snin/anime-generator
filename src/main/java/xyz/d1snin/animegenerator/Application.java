d2392a7498cbd49b8084d3116ac800e50996cd8f
d1snin <d1snin@aol.com>
2021-03-16 19:50:35 +0300

    [clean-up] clean up and some features

--- src/main/java/xyz/d1snin/animegenerator/Application.java
+++ src/main/java/xyz/d1snin/animegenerator/Application.java
@@ -1,38 +1,21 @@
 package xyz.d1snin.animegenerator;
 
-import java.util.Random;
-
 public class Application {
     public static void main(String[] args) {
-        Random random = new Random();
         try {
-            boolean isSaveMode = false;
             String tag = "";
-            try {
-                String saveMode = args[findIndex(args, "-s")];
-                isSaveMode = true;
-            } catch (InvalidSyntaxException invalidSyntaxException) {
-            }
             try {
                 tag = args[findIndex(args, "-t") + 1];
             } catch (InvalidSyntaxException invalidSyntaxException) {
             }
             int stringsCount = Integer.parseInt(args[findIndex(args, "-l") + 1]);
-            String fileName = null;
-            if (!isSaveMode) {
-                fileName = args[findIndex(args, "-n") + 1];
-            }
+            String fileName;
+            fileName = args[findIndex(args, "-n") + 1];
             System.out.println(" - Welcome!");
             System.out.println(" - Generating according to your parameters...");
-            if (!isSaveMode) {
-                Writer.write(ImageGen.getImages(stringsCount, tag), fileName);
-            } else {
-                ImageSave.downloadFile(ImageGen.getImages(stringsCount, tag), Integer.toString(random.nextInt(1000000) + 1000000));
-            }
+            Writer.write(ImageGen.getImages(stringsCount, tag), fileName);
         } catch (InvalidSyntaxException | NumberFormatException exception) {
-            System.out.println("Please use the following syntax:\njava -jar animegen.jar -l <count of generated images> -n <file name> -t <tag (optional)> -s <((optional) All pictures will be saved to the current folder)>");
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(" - Please use the following syntax:\njava -jar animegen.jar -l <count of generated images> -n <file name> -t <tag (optional)> -s <((optional) All pictures will be saved to the current folder)>");
         }
     }
     private static int findIndex(String[] arr, String toFind) throws InvalidSyntaxException {
