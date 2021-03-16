0c31bc10410f51a5613e9a878efa26a029680df1
d1snin <d1snin@aol.com>
2021-03-16 16:22:37 +0300

    [fix] fix

--- src/main/java/xyz/d1snin/animegenerator/Start.java
+++ src/main/java/xyz/d1snin/animegenerator/Start.java
@@ -2,8 +2,7 @@ package xyz.d1snin.animegenerator;
 
 public class Start {
     public static void main(String[] args) {
-        String fileName = args[0];
-        int strings = Integer.parseInt(args[1]);
+        int strings = Integer.parseInt(args[0]);
         System.out.println("Welcome!");
         System.out.println("Generating...");
         Generator.Generate(strings);
