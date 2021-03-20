b43c7235fa81afa8b7479b99196cdc701d6fd074
d1snin <d1snin@aol.com>
2021-03-20 21:29:24 +0300

    [clean-up] better code.

--- src/main/java/xyz/d1snin/animegenerator/Application.java
+++ src/main/java/xyz/d1snin/animegenerator/Application.java
@@ -4,9 +4,11 @@ public class Application {
     public static void main(String[] args) {
         try {
             boolean isSaveMode = false;
+            boolean isTagProvided = false;
             String tag = "";
             try {
                 tag = args[findIndex(args, "-t") + 1];
+                isTagProvided = true;
             } catch (InvalidSyntaxException invalidSyntaxException) {
                 /* ignore */
             }
@@ -17,11 +19,14 @@ public class Application {
                 /* ignore */
             }
             int stringsCount = Integer.parseInt(args[findIndex(args, "-l") + 1]);
-            String fileName;
-            fileName = args[findIndex(args, "-n") + 1];
+            String fileName = args[findIndex(args, "-n") + 1];
             System.out.println(" - Welcome, " + System.getProperty("user.name") + "!");
             System.out.println(" - Generating according to your parameters...");
             System.out.println(" - How much will be generated: " + stringsCount);
+            System.out.println(" - File name: " + fileName + ".json");
+            if (isSaveMode) System.out.println(" - The images will be saved to the current directory");
+            if (isTagProvided) System.out.println(" - Tag: " + tag);
+
             Writer.write(ImageGen.getImages(stringsCount, tag, isSaveMode), fileName);
             System.exit(0);
         } catch (InvalidSyntaxException | NumberFormatException exception) {
